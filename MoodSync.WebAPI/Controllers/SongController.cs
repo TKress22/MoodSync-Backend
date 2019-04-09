612043b79b7dc7aacb17493a8577340bf28178c0
VividerAphid <trever.kress@gmail.com>
2019-04-09 15:46:59 -0400

    Crud finished, rough GetSongBy() method added, working on it still

--- MoodSync.WebAPI/Controllers/SongController.cs
+++ MoodSync.WebAPI/Controllers/SongController.cs
@@ -11,11 +11,77 @@ namespace MoodSync.WebAPI.Controllers
 {
     public class SongController : ApiController
     {
-        public IHttpActionResult GetById(int id)
+        //public IHttpActionResult GetSongBy(object o)
+        //{
+        //    SongService service = CreateSongService();
+        //    Dictionary<string, dynamic> values = new Dictionary<string, dynamic>();
+        //    foreach (var p in o.GetType().GetProperties())
+        //    {
+        //        values.Add(p.Name, p.GetValue(o));
+        //    }
+        //    switch (values["type"])
+        //    {
+        //        case "Id":
+        //            return Ok(service.GetSongById(values["type"]));
+        //        case "genre":
+        //            return Ok(service.GetSongById(values["type"]));
+        //    }
+        //    return InternalServerError();
+        //}
+        public IHttpActionResult Post(SongCreate song)
         {
-            SongService songService = CreateSongService();
-            var song = songService.GetSongById(id);
-            return Ok(song);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateSongService();
+
+            if (!service.CreateSong(song))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Put(SongFetch song)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateSongService();
+
+            if (!service.UpdateSong(song))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateSongService();
+
+            if (!service.DeleteSong(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult GetSongBy(int type, string inp)
+        {
+            SongService service = CreateSongService();
+            switch (type)
+            {
+                case 0:
+                    return Ok(service.GetSongs());
+                case 1:
+                    return Ok(service.GetSongById(int.Parse(inp)));
+                case 2:
+                    return Ok(service.GetSongByGenre(int.Parse(inp)));
+                case 3:
+                    return Ok(service.GetSongByName(inp));
+                case 4:
+                    return Ok(service.GetSongByArtist(inp));
+                case 5:
+                    return Ok(service.GetSongByAlbum(inp));
+                case 6:
+                    return Ok(service.GetSongByChildFriendly(bool.Parse(inp)));
+            }
+            return Ok();
         }
         private SongService CreateSongService()
         {
