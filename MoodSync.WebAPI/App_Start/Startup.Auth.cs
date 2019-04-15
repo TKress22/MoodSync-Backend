6101c09ca06da837875e88421cedf8635896d182
lisajkilgore <lisajkilgore@gmail.com>
2019-04-15 14:54:04 -0400

    Cors install in Startup.Auth

--- MoodSync.WebAPI/App_Start/Startup.Auth.cs
+++ MoodSync.WebAPI/App_Start/Startup.Auth.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
@@ -45,7 +46,7 @@ namespace MoodSync.WebAPI
             };
 
             // Enable the application to use bearer tokens to authenticate users
-            app.UseOAuthBearerTokens(OAuthOptions);
+            app.UseCors(CorsOptions.AllowAll);
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
